4e6eab1417038a0c0c0106bf5f55825076d5abaa
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-05-09 16:38:32 -0500

    added functionality

---
+++ blog/blog.js
@@ -0,0 +1,12 @@
+"use strict"
+$(function() {
+
+
+    const myButton = document.getElementById("toggle");
+
+    myButton.addEventListener("click", function() {
+        document.body.style.backgroundColor = "black";
+    });
+
+});
+
