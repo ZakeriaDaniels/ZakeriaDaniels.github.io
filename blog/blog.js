b04268e359bb6b18201bd083cdd25b0285bb5e5a
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-05-09 22:17:34 -0500

    added double click functionality to lightbulb

--- blog/blog.js
+++ blog/blog.js
@@ -5,12 +5,12 @@ $(function() {
     const myButton = document.getElementById("toggle");
 
     myButton.addEventListener("click", function() {
-        document.body.style.backgroundColor = "black";
-    });
-
-    myButton.addEventListener("dblclick", function() {
-        document.body.style.backgroundColor = "white";
-    });
-
+        const currentColor = document.body.style.backgroundColor;
+        if (currentColor === "white") {
+            document.body.style.backgroundColor = "black";
+        } else {
+            document.body.style.backgroundColor = "white";
+        }
+    })
 });
 
